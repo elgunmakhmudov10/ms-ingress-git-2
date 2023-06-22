625f991899360f41b7c1251a39c3b7b5fa486d38
elgunmakhmudov10 <themaxmudov77@gmail.com>
2023-06-22 23:40:20 +0400

    ADDED NEW LINE

--- .idea/src/Fly.java
+++ .idea/src/Fly.java
@@ -2,6 +2,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Fly {
     public static void main(String[] args) {
-        System.out.println("DSadasdassaddddddd");
+        System.out.println("Bird can fly");
     }
 }
