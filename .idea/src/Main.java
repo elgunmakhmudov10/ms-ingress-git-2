95b1898591e8c0b2ab80c8988f09d45a59edafb8
elgunmakhmudov10 <137276769+elgunmakhmudov10@users.noreply.github.com>
2023-06-23 00:25:43 +0400

    Update Main.java

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Test");
+        System.out.println("1111Test1111");
         System.out.println("My name is Elgun");
     }
 }
