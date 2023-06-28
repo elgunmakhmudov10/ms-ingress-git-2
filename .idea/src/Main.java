f4361c94eda521e3ef87a96831fd899b06a8c61c
elgunmakhmudov10 <themaxmudov77@gmail.com>
2023-06-28 13:38:58 +0400

    hello ingress

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-         System.out.println("Hello worldsssssssssssssss12321");
+         System.out.println("1st1");
+        System.out.println();
+        System.out.println("Hello ingress");
     }
 }
