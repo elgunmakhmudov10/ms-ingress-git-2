f220d354963281ae629e03d911988bbaef893103
elgunmakhmudov10 <themaxmudov77@gmail.com>
2023-06-29 14:47:01 +0400

    hello ingressss

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world");
          System.out.println("1st1");
         System.out.println();
-        System.out.println("Hello ingress");
+        System.out.println("Hello ingresss");
     }
 }
